a851cc74591a8c9e93a2cb017c6390aae4722330
thzzcosta <50201654+thzzcosta@users.noreply.github.com>
2026-03-29 18:57:52 -0300

    Atualizando interfaces globais e configurando Firebase

--- AquaSense/types/index.ts
+++ AquaSense/types/index.ts
@@ -1,2 +1,5 @@
 // Arquivo de entrada para tipos e interfaces globais do projeto.
 // Futuramente: centralizar aqui os contratos compartilhados entre telas, serviços e contextos.
+
+export * from './user';
+export * from './teams';
