29ebf0f059d34674c3cd0932c377561fe72ebd12
eianaxz <asilvasouza264@gmail.com>
2026-03-29 22:38:29 -0300

    feat: redireciona tela inicial para seleção de perfil

---
+++ AquaSense/app/index.tsx
@@ -0,0 +1,11 @@
+import { Redirect } from "expo-router";
+
+/**
+ * Ponto de entrada principal do app.
+ * Redireciona automaticamente para a tela de seleção de perfil.
+ */
+export default function Index() {
+    return <Redirect href="/select_user_type"/>;
+}
+
+
